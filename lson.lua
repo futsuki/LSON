ffb200303e6ab52995cbe93ddec3abcc90f536b6
Futsuki <gcc111014+git@gmail.com>
2013-11-28 13:05:33 +0900

    てきとうなLUAオブジェクト表示用 or 保存法

---
+++ lson.lua
@@ -0,0 +1,131 @@
+local self = getfenv()
+local loadstring, type, ipairs, pairs, pcall, print, getmetatable, setmetatable
+loadstring, type, ipairs, pairs, pcall, print, getmetatable, setmetatable = self.loadstring, self.type, self.ipairs, self.pairs, self.pcall, self.print, self.getmetatable, self.setmetatable
+local byte, sub
+do
+  local _obj_0 = string
+  byte, sub = _obj_0.byte, _obj_0.sub
+end
+local toLSON
+toLSON = function(o, reconstructable, circularCheckTable)
+  if reconstructable == nil then
+    reconstructable = true
+  end
+  if circularCheckTable == nil then
+    circularCheckTable = { }
+  end
+  if type(o) == "table" then
+    if circularCheckTable[o] ~= nil then
+      if reconstructable then
+        error("Converting circular structure to LSON")
+      else
+        return "[circular reference]"
+      end
+    else
+      circularCheckTable[o] = true
+    end
+  end
+  local _exp_0 = type(o)
+  if "table" == _exp_0 then
+    local first = true
+    local ret = "{"
+    local processed = -1
+    for i, v in ipairs(o) do
+      if first then
+        first = false
+      else
+        ret = ret .. ", "
+      end
+      local vstr = toLSON(v, reconstructable, circularCheckTable)
+      ret = ret .. tostring(vstr)
+      processed = i
+    end
+    for k, v in pairs(o) do
+      local _continue_0 = false
+      repeat
+        if type(k) == "number" then
+          if k >= 1 and k <= processed then
+            _continue_0 = true
+            break
+          end
+        end
+        if reconstructable and (type(v) == "thread" or type(v) == "userdata") then
+          _continue_0 = true
+          break
+        end
+        if first then
+          first = false
+        else
+          ret = ret .. ", "
+        end
+        local vstr = toLSON(v, reconstructable, circularCheckTable)
+        local kstr = toLSON(k, reconstructable, circularCheckTable)
+        if vstr ~= nil then
+          if reconstructable then
+            ret = ret .. "[" .. tostring(kstr) .. "] = " .. tostring(vstr)
+          else
+            ret = ret .. tostring(kstr) .. " = " .. tostring(vstr)
+          end
+        else
+          ret = ret .. "[" .. tostring(kstr) .. "] = nil"
+        end
+        _continue_0 = true
+      until true
+      if not _continue_0 then
+        break
+      end
+    end
+    ret = ret .. "}"
+    return ret
+  elseif "string" == _exp_0 then
+    if reconstructable then
+      if o:byte(1) == 36 then
+        return "\"$" .. tostring(o) .. "\""
+      else
+        return "\"" .. tostring(o) .. "\""
+      end
+    else
+      return tostring(o)
+    end
+  elseif "function" == _exp_0 then
+    if reconstructable then
+      local stat, str = pcall(string.dump, o)
+      if stat and str ~= nil then
+        return "loadstring(base64.decode(\"" .. tostring(base64.encode(str)) .. "\"))"
+      else
+        return nil
+      end
+    else
+      return tostring(o)
+    end
+  else
+    return tostring(o)
+  end
+end
+local fromLSON
+fromLSON = function(str)
+  local chunk = loadstring("return (" .. tostring(str) .. ");")
+  local stat, ret = pcall(chunk)
+  if stat then
+    return ret
+  else
+    return nil
+  end
+end
+self.LSON = {
+  stringify = toLSON,
+  parse = fromLSON
+}
+self.p = function(...)
+  local param = {
+    ...
+  }
+  if #param == 1 then
+    print(toLSON(param[1], false))
+  else
+    for i, v in ipairs(param) do
+      print(toLSON(v, false))
+    end
+  end
+  return ...
+end
---
+++ lson.moon
@@ -0,0 +1,102 @@
+
+@ = getfenv()
+
+import loadstring, type, ipairs, pairs, pcall, print, getmetatable, setmetatable from @
+import byte, sub from string
+
+-- lua script object notation
+toLSON = (o, reconstructable=true, circularCheckTable={}) ->
+    if type(o) == "table"
+        if circularCheckTable[o] != nil
+            if reconstructable
+                error "Converting circular structure to LSON"
+            else
+                return "[circular reference]"
+        else
+            circularCheckTable[o] = true
+    switch type(o)
+        when "table"
+            first = true
+            ret = "{"
+            processed = -1
+            for i, v in ipairs o
+                if first
+                    first = false
+                else
+                    ret ..= ", "
+                vstr = toLSON v, reconstructable, circularCheckTable
+                ret ..= "#{vstr}"
+                processed = i
+            for k, v in pairs o
+                if type(k) == "number"
+                    if k >= 1 and k <= processed
+                        continue
+                if reconstructable and (type(v) == "thread" or type(v) == "userdata")
+                    continue
+                if first
+                    first = false
+                else
+                    ret ..= ", "
+                vstr = toLSON v, reconstructable, circularCheckTable
+                kstr = toLSON k, reconstructable, circularCheckTable
+                if vstr != nil
+                    if reconstructable
+                        ret ..= "[#{kstr}] = #{vstr}"
+                    else
+                        ret ..= "#{kstr} = #{vstr}"
+                else
+                    ret ..= "[#{kstr}] = nil"
+            ret ..= "}"
+            ret
+        when "string"
+            if reconstructable
+                if o\byte(1) == 36
+                    "\"$#{o}\""
+                else
+                    "\"#{o}\""
+            else
+                "#{o}"
+        when "function"
+            if reconstructable
+                stat, str = pcall(string.dump, o)
+                if stat and str != nil
+                    --"\"$"..base64.encode(LibCompress\Compress(str)).."\""
+                    --"\"$"..base64.encode(str).."\""
+                    "loadstring(base64.decode(\"#{base64.encode(str)}\"))"
+                else
+                    nil
+            else
+                "#{o}"
+        else
+            "#{o}"
+
+fromLSON = (str) ->
+    chunk = loadstring("return (#{str});")
+    stat, ret = pcall(chunk)
+    if stat
+        ret
+    else
+        nil
+
+
+@LSON = 
+    stringify: toLSON
+    parse: fromLSON
+
+-- pretty print
+@p = (...) ->
+    param = {...}
+    if #param == 1
+        print toLSON(param[1], false)
+    else
+        for i, v in ipairs param
+            print toLSON(v, false)
+    ...
+
+
+
+
+
+
+
+
